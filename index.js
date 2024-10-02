d3b0e9d81a465d4bbf82da16b379881a13911cf5
Barrington Hebert <bondman2004tippy@gmail.com>
2024-10-02 10:40:59 -0500

    aye

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ $(document).ready(() => {
   const $body = $('body');
   $body.html('');
 
-  const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
-    const text = `@${tweet.user}: ${tweet.message}`;
+  function createTweets() {
+    const $tweets = streams.home.map((tweet) => {
+      const $tweet = $('<div></div>');
+      const text = `@${tweet.user}: ${tweet.message}`; //this part will also change, something about being clickable
 
-    $tweet.text(text);
+      $tweet.text(text); //we will add a timestamp to this
 
-    return $tweet;
-  });
+      return $tweet;
+    });
   $body.append($tweets);
-
+  }
 });
