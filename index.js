3f3f60badd3342e549a65180dd27ea791d787ede
Barrington Hebert <bondman2004tippy@gmail.com>
2024-10-02 17:01:19 -0500

    got tweets to show on page

--- index.js
+++ index.js
@@ -6,18 +6,35 @@ streams.users is an object with properties for each user. streams.users.shawndro
 $(document).ready(() => {
   const $body = $('body');
   $body.html('');
+  $logo = $('<h1 id="logo"></h1>')
+          .text("Twiddler");
+  $section = $('<section id="Tweets Section"></section>')
+  $div = $('<div id: Tweets Div></div>')
+  createTweets(); //calls the function create tweets
+  $body.prepend($logo); //places logo at the top
+  $body.append($section) //places section into the body at the bottom
+  $section.appendTo($div) //places the section into the div
+  $body.append($div) //has the body place the div at the bottom
+
 
   function createTweets() {
     const $tweets = streams.home.map((tweet) => {
-      const $tweet = $('<div></div>');
-      const text = `@${tweet.user}: ${tweet.message}`; //this part will also change, something about being clickable
+      let timeNow = new Date();
+      let hr = timeNow.getHours();
+      let min = timeNow.getMinutes();
+      const $tweet = $(`<div id=${tweet.user} class="user"></div>`);
+      const text = `@${tweet.user}: ${tweet.message} ${hr}:${min}`; //this part will also change, something about being clickable
 
       $tweet.text(text); //we will add a timestamp to this
-
+      
       return $tweet;
     });
 
-  $body.append($tweets);
+    
+
+  $section.append($tweets);
   }
-  
+  $('.user').on('click', () => {
+    console.log('clicked')
+ })
 });
