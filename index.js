63893b5c07685e01df0627bb916c423c1d65978f
Barrington Hebert <bondman2004tippy@gmail.com>
2024-10-03 10:58:56 -0500

    got clicked tweets to work

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ $(document).ready(() => {
   $logo = $('<h1 id="logo"></h1>')
           .text("Twiddler");
   $section = $('<section id="Tweets Section"></section>')
-  $div = $('<div id: Tweets Div></div>')
+  $div = $('<div id=Tweets Div></div>')
   createTweets(); //calls the function create tweets
   $body.prepend($logo); //places logo at the top
   $body.append($section) //places section into the body at the bottom
@@ -26,8 +26,8 @@ $(document).ready(() => {
       const text = `@${tweet.user}: ${tweet.message} ${hr}:${min}`; //this part will also change, something about being clickable
       //$tweet.prepend($link2Page)
       $tweet.text(text); //we will add a timestamp to this
-      const $link2Page = $(`<a href="raccooncitymassacre.bandcamp.com">@${tweet.user}</a>`)
-      $tweet.prepend($link2Page)
+      //const $link2Page = $(`<a href="raccooncitymassacre.bandcamp.com">@${tweet.user}</a>`)
+      //$tweet.prepend($link2Page)
       return $tweet;
     });
 
@@ -35,12 +35,33 @@ $(document).ready(() => {
 
   $section.append($tweets);
   }
-  $('.user').on('click', () => {
+  $('.user').click( function () {
     console.log('clicked')
+    $section.remove();
+    let theUser = $(this).attr('id')
+    console.log(theUser)
+    const $clickedTweets = streams.users[theUser].map((tweet) => {
+      let timeNow = new Date();
+      let hr = timeNow.getHours();
+      let min = timeNow.getMinutes();
+      const $clickedTweets = $(`<div id=${tweet.user} class="user"></div>`);
+      const text = `@${tweet.user}: ${tweet.message} ${hr}:${min}`; //this part will also change, something about being clickable
+      //$tweet.prepend($link2Page)
+      $clickedTweets.text(text); //we will add a timestamp to this
+      //const $link2Page = $(`<a href="raccooncitymassacre.bandcamp.com">@${tweet.user}</a>`)
+      //$tweet.prepend($link2Page)
+      return $clickedTweets;
+    });
+    //$userTweetsSection = $(`<section id="userTweets">${streams.users[theUser]}</section>`)
+    //.attr('id="userTweets"')
+    //.text(`hello World`) 
+    //${streams.users[theUser]}
+   // console.log($userTweetsSection)
+    $div.append($clickedTweets)
  })
 
-$('.user').append($link2Page)
-  $('.user').click(() => {
-    console.log('clicked')
-})
+//$('.user').append($link2Page)
+ // $('.user').click(() => {
+  //  console.log('clicked')
+//})
 });
