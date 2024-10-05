5cd46cfa014fcc0b1a49493d5259bb2ec0c2f25c
Barrington Hebert <bondman2004tippy@gmail.com>
2024-10-04 21:26:54 -0500

    8-bit Twiddler version 1

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ let timeNow = new Date();
 let hr = timeNow.getHours();
 let min = timeNow.getMinutes();
 const now1 = moment();
-
+$styleRequest1 = $('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Silkscreen"></link>')
+  
 $(document).ready(() => {
-  //declare a variable for theUser
+
+  $('head').attr('id', 'head');
+  //import a font
+  $styleRequest = $('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Silkscreen"></link>')
+  $('#head').append($styleRequest1)
+  //declare a variable for the user
   let theUser;
   //initialize variable x to 60 (this will be used for time)
   let x = 60
@@ -25,14 +31,22 @@ $(document).ready(() => {
   $body.html('');
   //create a header for the logo
   $logo = $('<h1 id="logo"></h1>')
+
   //make its text content say "twiddler"
-  .text("Twiddler");
+  .text("8-bit Twiddler")
+  .css('color', 'purple')
+  .css({ background: "-webkit-gradient(linear, left top, left bottom, from(#3CA125), to(rgb(255, 247, 0)))" })
+  
   //initialize a variable to a section for tweets
   $section = $('<section id="Tweets Section"></section>');
   //initialize a variable to a div for tweets
   $div = $('<div id=Tweets Div></div>')
    //places logo at the top
   $body.prepend($logo);
+  //give body an id
+  $body.attr('id', "body");
+  //create backgroudn image
+  $('#body').css('background-image', 'url(//f4.bcbits.com/img/0013829762_130.jpg)');
   //places section into the body at the bottom
   $body.append($section) 
   //places the section into the div
@@ -40,6 +54,7 @@ $(document).ready(() => {
   //has the body place the div at the bottom
   $body.append($div) 
   //call the function to create tweets
+  $body.prepend($styleRequest)
   createTweets()
   
     function createTweets(array) {
@@ -65,20 +80,36 @@ $(document).ready(() => {
       //freeze a moment in time
       const timeAgo = frozenMoment.fromNow()
       //set the tweet to be a div with class "user"
-      const $tweet = $(`<div id=${tweet.user} class="user"></div>`);
+      const $tweet = $(`<div id=${tweet.user} class="user" style="font-family: Silkscreen; width:800px; margin:0 auto;" ></div>`);
       //set a variable to text content of the username and message
-      const text = `@${tweet.user}: ${tweet.message}`; //this part will also change, something about being clickable
+      const $divText = $(`<div id="message&user"><strong>@${tweet.user}:</strong> ${tweet.message}></div>`); //this part will also change, something about being clickable
       //add this text to the tweet div
-      $tweet.text(text); //we will add a timestamp to this
+      $tweet.append($divText); //we will add a timestamp to this
+      
       //set up a variable equal to a div containing the time posted
       const $timePosted = $(`<div id="${frozenMoment}" class="time"> ${frozenMoment.format('MMMM Do YYYY, h:mm:ss a')}</div>`) 
       //set up variable equal to div of how long ago its been posted. set id to variable x
-      const $timeAgo = $(`<div id=${x} class="timeAgo"> ${timeAgo}</div>`)
+      const $timeAgo = $(`<div id=${x} class="timeAgo" style="font-family: Silkscreen;"> ${timeAgo}</div>`)
       //append that div to the tweet div
       $timeAgo.appendTo($tweet);
       //prepend the div time posted to the tweet div
       $tweet.prepend($timePosted);
       //return the tweet value
+      //give the tweet style
+      $tweet.css("color", "yellow")
+      $tweet.css("border-style", 'outset');
+      $tweet.css("border-color", "purple")
+      $tweet.css("background-color", "blue")
+      $timeAgo.css("font-size", "14px")
+      $divText.css("font-size", "15px")
+      $timePosted.css("font-size", "10")
+      $timeAgo.css({"font-style": "italic"})
+      $tweet.width(600);
+      $tweet.height(100);
+    
+      //$timePosted.css('float', 'left')
+      
+      
       return $tweet;
     });
 
@@ -89,7 +120,7 @@ $(document).ready(() => {
       //reassign theUser variable to this objects id tag
       theUser = $(this).attr('id')
       //destroy all divs that do not have that users id
-      $(`#${theUser}`).siblings().empty();
+      $(`#${theUser}`).siblings().remove();
       //change the clicked variable to true
       iWasClicked = true;
       //remove the ability to create new tweets
@@ -101,7 +132,7 @@ $(document).ready(() => {
     })
   };
 //set up created tweets to appear every few seconds
-setInterval(createTweets, 7000); //for now this is cool
+setInterval(createTweets, 4000); //for now this is cool
   //this setInterval function will be how we keep track of time
   setInterval( function(){
     //check to see if function is triggering and this will help tell when it resets
@@ -127,7 +158,8 @@ setInterval(createTweets, 7000); //for now this is cool
   //have this setInterval function run every second. 
   }, 1000)
   //create a container for the new section that will take in posts
-  $newPostsSection = ('<section id="newPostsSection" class="newposts"></section>');
+  $newPostsSection = ('<section id="newPostsSection"  class="newposts" ></section>');
+  $("<br>").appendTo($newPostsSection);
   //pass this section into the post-maker section
   postMaker($newPostsSection)
   //create the function that will take sections for the posts and add them to the top
@@ -138,24 +170,49 @@ setInterval(createTweets, 7000); //for now this is cool
     //create a form tag is used to collect information a user provides
     const $form = $("<form>").appendTo($container);
     //the <label> tag is used to create a label for where the username will go
-    $("<label for='postTitle'>UserName:</label>").appendTo($form);
+    $labelUserName = $("<label for='postTitle' style='font-family: Silkscreen;'>8-bit UserName:</label>")
+    $labelUserName.appendTo($form);
     //use the <input> tag to create an area to put the username, append it to the form
-    $("<input type='text' id='UserInput' name='title'>").appendTo($form);
+    $inputUserName = $("<input type='text' id='UserInput' name='title' style='font-family: Silkscreen;'>")
+    $inputUserName.appendTo($form);
     //use the <br> tag for a line break and append it to the form
     $("<br>").appendTo($form);
     //we prepend this container to the top of the $div section for tweets
     $div.prepend($container)
+    $("<br>").appendTo($labelUserName)
+    
     //the label tag is used to label a part of a form, here we label a part twiit
-    $("<label for='postContent'>Twiit:</label>").appendTo($form);
+    $labelContent = $("<label for='postContent' style='font-family: Silkscreen;'>8-bit Twidd:</label>")
+    $labelContent.appendTo($form);
+    $("<br>").appendTo($labelContent)
     //the <textarea> tag creates a multi-line plain text editing area
-    $("<textarea id='postContent' name='content'></textarea>").appendTo($form);
+    $textAreaContent = $("<textarea id='postContent' name='content' style='font-family: Silkscreen;'></textarea>")
+    $textAreaContent.appendTo($form);
     //the <br> tag is to create a line break without creating a new paragraph
     $("<br>").appendTo($form);
+
     //create a button
     $button = $("<button type='submit'>Twiit It Baby!</button>")
     //append the button to the form
     $button.appendTo($form);
-  
+    //$button.css("text-align", "center")
+    $container.css('color', 'yellow')
+    $container.css("border-style", 'outset');
+    $container.css("border-color", "purple")
+    $container.css("background-color", "blue")
+    $container.width(300);
+    $container.height(150);
+    $container.css({
+      "margin-left": "auto",
+      "margin-right": "auto"
+    })
+    $button.css('background-color', 'green')
+    $textAreaContent.css('color', 'white')
+    $textAreaContent.css('background-color', 'black')
+    $textAreaContent.parent().css("text-align", "center");
+    $inputUserName.css('color', 'white')
+    $inputUserName.css('background-color', 'black')
+    
     // this function prevents the form from refreshing the page, when the submit button is pushed
     $form.submit(function(event) {
       //it prevents the page from refreshing
@@ -176,10 +233,13 @@ setInterval(createTweets, 7000); //for now this is cool
       //and a message key with the value of the content
       streams.users[title] = [{ user: title, message: content}]
       //call the create tweets function passing in this value
-      setTimeout(createTweets(streams.users[title]), 0);
+      createTweets(streams.users[title]);
       //delete the created object
       delete streams.users[title]
     })
+
+    //styling the button
+    
   }
 
 });
